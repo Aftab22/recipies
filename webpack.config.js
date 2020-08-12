cbf6e792e93ba0402e0f9b713775ab35f3eec3fa
Aftab Faisal <aftab.22.1996@gmail.com>
2020-08-12 15:24:03 +0530

    bundle.js setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = {
   entry: "./src/js/index.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/js"),
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
