1b31eb2022ae6027d08d873d3d5cbeca3a6e2682
Aftab Faisal <aftab.22.1996@gmail.com>
2020-08-16 21:22:38 +0530

    Search model set up / async babel fix / 1st api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-console.warn("hello world")
+import Search from "./models/Search";
+
+// Global State of our app
+// -Search Object
+// -Current ObjectShopping List
+// -Liked Recipies
+const state = {};
+
+const controlSearch = async () => {
+    //1.get search query
+    const query = "pizza";
+    if (query) {
+        //2.create new search object and add to state
+        state.search = new Search(query);
+        //3.Prepare the UI for search results
+        //4.perform search api call for search object
+        await state.search.getResults();
+        //5.Render the result of API call
+        console.warn(state.search.results);
+    }
+};
+
+document.querySelector(".search").addEventListener("submit", (event) => {
+    event.preventDefault();
+    controlSearch();
+});
+
+const search = new Search("pizza");
+search.getResults();
